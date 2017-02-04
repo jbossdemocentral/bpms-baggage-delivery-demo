57a24061aff69c2dacebe591c71f426cb4f74d36
Eric D. Schabell <eric@schabell.org>
2017-02-04 19:58:10 +0100

    Updated to BPM Suite 6.4 and EAP 7.0.

--- init.sh
+++ init.sh
@@ -1,19 +1,18 @@
 #!/bin/sh 
 DEMO="Baggage Delivery Demo"
 AUTHORS="Jason Milliron, Andrew Block, Eric D. Schabell"
-PROJECT="git@github.com:jbossdemocentral/bpms-baggage-delivery-demo.git"
+PROJECT="git@github.com:eschabell/bpms-baggage-delivery-demo.git"
 PRODUCT="JBoss BPM Suite"
-JBOSS_HOME=./target/jboss-eap-6.4
+JBOSS_HOME=./target/jboss-eap-7.0
 SERVER_DIR=$JBOSS_HOME/standalone/deployments/
 SERVER_CONF=$JBOSS_HOME/standalone/configuration/
 SERVER_BIN=$JBOSS_HOME/bin
 SRC_DIR=./installs
 SUPPORT_DIR=./support
 PRJ_DIR=./projects
-BPMS=jboss-bpmsuite-installer-6.2.0.BZ-1299002.jar
-EAP=jboss-eap-6.4.0-installer.jar
-EAP_PATCH=jboss-eap-6.4.4-patch.zip
-VERSION=6.2
+BPMS=jboss-bpmsuite-6.4.0.GA-deployable-eap7.x.zip
+EAP=jboss-eap-7.0.0-installer.jar
+VERSION=6.4
 
 # wipe screen.
 clear 
@@ -34,7 +33,7 @@ echo "##                                                                 ##"
 echo "##  brought to you by,                                             ##"   
 echo "##       ${AUTHORS}            ##"
 echo "##                                                                 ##"   
-echo "##  ${PROJECT} ##"
+echo "##  ${PROJECT}        ##"
 echo "##                                                                 ##"   
 echo "#####################################################################"
 echo
@@ -43,7 +42,7 @@ command -v mvn -q >/dev/null 2>&1 || { echo >&2 "Maven is required but not insta
 
 # make some checks first before proceeding.	
 if [ -r $SRC_DIR/$EAP ] || [ -L $SRC_DIR/$EAP ]; then
-	echo Product sources are present...
+	echo Product EAP sources are present...
 	echo
 else
 	echo Need to download $EAP package from the Customer Portal 
@@ -52,18 +51,8 @@ else
 	exit
 fi
 
-if [ -r $SRC_DIR/$EAP_PATCH ] || [ -L $SRC_DIR/$EAP_PATCH ]; then
-	echo Product patches are present...
-	echo
-else
-	echo Need to download $EAP_PATCH package from the Customer Portal 
-	echo and place it in the $SRC_DIR directory to proceed...
-	echo
-	exit
-fi
-
 if [ -r $SRC_DIR/$BPMS ] || [ -L $SRC_DIR/$BPMS ]; then
-		echo Product sources are present...
+		echo Product BPM Suite sources are present...
 		echo
 else
 		echo Need to download $BPMS package from the Customer Portal 
@@ -91,14 +80,9 @@ if [ $? -ne 0 ]; then
 fi
 
 echo
-echo "Applying JBoss EAP 6.4.4 patch now..."
-echo
-$JBOSS_HOME/bin/jboss-cli.sh --command="patch apply $SRC_DIR/$EAP_PATCH"
-
-echo
 echo "JBoss BPM Suite installer running now..."
 echo
-java -jar $SRC_DIR/$BPMS $SUPPORT_DIR/installation-bpms -variablefile $SUPPORT_DIR/installation-bpms.variables
+unzip -qo $SRC_DIR/$BPMS -d ./target
 
 if [ $? -ne 0 ]; then
 	echo Error occurred during $PRODUCT installation!
@@ -106,9 +90,9 @@ if [ $? -ne 0 ]; then
 fi
 
 echo
-echo "  - enabling demo accounts role setup in application-roles.properties file..."
+echo "  - enabling demo account setup..."
 echo
-cp $SUPPORT_DIR/application-roles.properties $SERVER_CONF
+$JBOSS_HOME/bin/add-user.sh -a -r ApplicationRealm -u erics -p bpmsuite1! -ro analyst,admin,manager,user,kie-server,kiemgmt,rest-all --silent
 
 echo "  - setting up demo projects..."
 echo
@@ -116,7 +100,6 @@ cp -r $SUPPORT_DIR/bpm-suite-demo-niogit $SERVER_BIN/.niogit
 
 echo "  - setting up zip code services..."
 echo
-#cp -r $SUPPORT_DIR/ZipCodeServices.war $SERVER_DIR
 mvn clean install -f $PRJ_DIR/ZipCodeServices/pom.xml
 cp $PRJ_DIR/ZipCodeServices/target/ZipCodeServices-1.0.war $SERVER_DIR
 
@@ -152,6 +135,6 @@ echo "=    http://localhost:8080/business-central  (u:erics / p:bpmsuite1!)  ="
 echo "=                                                                      ="
 echo "=  See README.md for general details to run the various demo cases.    ="
 echo "=                                                                      ="
-echo "=  $PRODUCT $VERSION $DEMO Setup Complete.            ="
+echo "=  $PRODUCT $VERSION $DEMO Setup Complete.           ="
 echo "=                                                                      ="
 echo "========================================================================"
